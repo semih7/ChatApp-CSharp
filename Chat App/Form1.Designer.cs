aef316ef5f13d74509bedf04f251d8d7692f96ed
semih7 <semih.siyahdemirrr@gmail.com>
2019-12-18 18:25:10 +0300

    Simple Chat App

---
+++ Chat App/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Chat_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.HostUserName = new System.Windows.Forms.TextBox();
+            this.KullanıcıAdı1 = new System.Windows.Forms.Label();
+            this.textLocalPort = new System.Windows.Forms.TextBox();
+            this.textLocalIP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textFriendsIP = new System.Windows.Forms.TextBox();
+            this.textFriendsPort = new System.Windows.Forms.TextBox();
+            this.FriendUserName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listMessage = new System.Windows.Forms.ListBox();
+            this.textMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.HostUserName);
+            this.groupBox1.Controls.Add(this.KullanıcıAdı1);
+            this.groupBox1.Controls.Add(this.textLocalPort);
+            this.groupBox1.Controls.Add(this.textLocalIP);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(38, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(321, 151);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sizin Bilgileriniz";
+            this.groupBox1.Enter += new System.EventHandler(this.GroupBox1_Enter);
+            // 
+            // HostUserName
+            // 
+            this.HostUserName.Location = new System.Drawing.Point(198, 123);
+            this.HostUserName.Name = "HostUserName";
+            this.HostUserName.Size = new System.Drawing.Size(100, 22);
+            this.HostUserName.TabIndex = 4;
+            this.HostUserName.TextChanged += new System.EventHandler(this.HostUserName_TextChanged);
+            // 
+            // KullanıcıAdı1
+            // 
+            this.KullanıcıAdı1.AutoSize = true;
+            this.KullanıcıAdı1.Location = new System.Drawing.Point(6, 123);
+            this.KullanıcıAdı1.Name = "KullanıcıAdı1";
+            this.KullanıcıAdı1.Size = new System.Drawing.Size(102, 17);
+            this.KullanıcıAdı1.TabIndex = 3;
+            this.KullanıcıAdı1.Text = "Kullanıcı Adınız";
+            // 
+            // textLocalPort
+            // 
+            this.textLocalPort.Location = new System.Drawing.Point(198, 79);
+            this.textLocalPort.Name = "textLocalPort";
+            this.textLocalPort.Size = new System.Drawing.Size(100, 22);
+            this.textLocalPort.TabIndex = 1;
+            // 
+            // textLocalIP
+            // 
+            this.textLocalIP.Location = new System.Drawing.Point(198, 32);
+            this.textLocalIP.Name = "textLocalIP";
+            this.textLocalIP.Size = new System.Drawing.Size(100, 22);
+            this.textLocalIP.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textFriendsIP);
+            this.groupBox2.Controls.Add(this.textFriendsPort);
+            this.groupBox2.Controls.Add(this.FriendUserName);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(399, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(337, 151);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Arkadaşınızın Bilgileri";
+            // 
+            // textFriendsIP
+            // 
+            this.textFriendsIP.Location = new System.Drawing.Point(213, 37);
+            this.textFriendsIP.Name = "textFriendsIP";
+            this.textFriendsIP.Size = new System.Drawing.Size(100, 22);
+            this.textFriendsIP.TabIndex = 1;
+            // 
+            // textFriendsPort
+            // 
+            this.textFriendsPort.Location = new System.Drawing.Point(213, 79);
+            this.textFriendsPort.Name = "textFriendsPort";
+            this.textFriendsPort.Size = new System.Drawing.Size(100, 22);
+            this.textFriendsPort.TabIndex = 2;
+            // 
+            // FriendUserName
+            // 
+            this.FriendUserName.Location = new System.Drawing.Point(213, 123);
+            this.FriendUserName.Name = "FriendUserName";
+            this.FriendUserName.Size = new System.Drawing.Size(100, 22);
+            this.FriendUserName.TabIndex = 3;
+            this.FriendUserName.TextChanged += new System.EventHandler(this.FriendUserName_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(172, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Arkadaşınızın Kullanıcı Adı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "IP";
+            // 
+            // listMessage
+            // 
+            this.listMessage.FormattingEnabled = true;
+            this.listMessage.ItemHeight = 16;
+            this.listMessage.Location = new System.Drawing.Point(38, 179);
+            this.listMessage.Name = "listMessage";
+            this.listMessage.Size = new System.Drawing.Size(698, 196);
+            this.listMessage.TabIndex = 5;
+            // 
+            // textMessage
+            // 
+            this.textMessage.Location = new System.Drawing.Point(38, 381);
+            this.textMessage.Name = "textMessage";
+            this.textMessage.Size = new System.Drawing.Size(698, 22);
+            this.textMessage.TabIndex = 6;
+            this.textMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textMessage_KeyDown);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(742, 380);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(742, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(742, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Renk";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.textMessage);
+            this.Controls.Add(this.listMessage);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox HostUserName;
+        private System.Windows.Forms.Label KullanıcıAdı1;
+        private System.Windows.Forms.TextBox textLocalPort;
+        private System.Windows.Forms.TextBox textLocalIP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textFriendsIP;
+        private System.Windows.Forms.TextBox textFriendsPort;
+        private System.Windows.Forms.TextBox FriendUserName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listMessage;
+        private System.Windows.Forms.TextBox textMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Chat App/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;                           //System.net modülünü kullanacağımız için bu modülü ekledik.
+using System.Net.Sockets;                   //Aynı zamanda soketleri kullanacağımız için bu modülü de ekledik.
+
+namespace Chat_App
+{
+    public partial class Form1 : Form
+    {
+        Socket sck;                     //Socket ve endpoint tanımlamaları yapıldı.
+        EndPoint epLocal, epRemote;     //epLocal ve epRemote tanımlamaları yapıldı. (Endpointler)
+        
+        public Form1()
+        {
+            InitializeComponent();
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            /* socket oluşturuldu adress family internetwork olarak ayarlandı soket tipi datagram ve protokol tipi
+             udp olarak ayarlandı. Soket ayarlamaları yapıldı. 
+            */
+
+            textLocalIP.Text = GetLocalIP();            //Oluşturulan GetLocalIP fonksiyonu LocalIP ve FriendsIP
+            textFriendsIP.Text = GetLocalIP();          //Bölümlerine yazdırıldı.
+        }
+
+        private string GetLocalIP()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if(ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();                   //Alınan ip adresi her iki durum için de yazılmak için foreach fonksiyonu ile açıldı.
+                }                                           //Alınan ip adresinin adres ailesi internetwork'e eşitse alınan ip adresini string veri tipine dönüştürdük.
+            }                                               //String veri tipine dönüştürmemizin sebebi textbox bölümüne yazdırabilmek için
+            return "127.0.0.1";                             //Eğer if koşuluna girmezse return değerini ayarladık. 
+        }
+
+        private void MessageCallBack(IAsyncResult aResult)
+        {
+            try
+            {
+                int size = sck.EndReceiveFrom(aResult, ref epRemote);       //Mesaj gönderebilmek için MessageCallBack fonksiyonunu oluşturduk.
+                if(size > 0)                                                //Gönderilen mesajın boyu 0'dan büyükse yani boş mesaj değilse koşulunu oluşturduk.
+                {                                   
+                    byte[] receivedData = new byte[1400];                   //iletilecek olan mesajı byte türüne dönüştürdük.
+                    receivedData = (byte[])aResult.AsyncState;              //Arka arkaya 1 den fazla mesaj gönderilmesi koşulunu ayarladık.
+                    ASCIIEncoding eEncoding = new ASCIIEncoding();          //Girilen mesajı ASCII ile kodladık.
+
+                    string receivedMessage = eEncoding.GetString(receivedData);     //Gönderilen mesajı tekrar string tipine dönüştürdük bu sayede listboxa ekleme yapabileceğiz.
+                    listMessage.Items.Add(FriendUserName.ToString() + " => " + receivedMessage);             //Listbox'a gönderilen mesajlar eklendi ve her iki kullanıcının görmesi sağlandı. 
+
+                }
+                byte[] buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+
+                //Yeni mesaj göndeimi için buffer boşaltıldı. soket parametreleri girildi.
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.ToString());        //Hata yakalaması yapıldı. Alınan hata messagebox üzerinde gösterildi.
+
+            }
+        }
+        
+        private void GroupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                epLocal = new IPEndPoint(IPAddress.Parse(textLocalIP.Text), Convert.ToInt32(textLocalPort.Text));
+                sck.Bind(epLocal);
+
+                //Local endpoint oluşturuldu. Oluşturulan endpoint ile soket eşleştirildi.
+
+                epRemote = new IPEndPoint(IPAddress.Parse(textFriendsIP.Text), Convert.ToInt32(textFriendsPort.Text));
+                sck.Connect(epRemote);
+
+                //Remote endpoint oluşturuldu. Oluşturulan endpoint ile soket bağlanması gerçekleştirildi.
+
+                byte[] buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+                
+                //Gönderilecek mesaj için 1500 byte'lık yer açıldı. Soket üzerinden mesaj alımı sağlandı. 
+
+                button1.Text = "Connected";         //Start butonu basıldığında içine connected yazması sağlandı. 
+                button1.Enabled = false;            //Bu yazımdan sonra buton inaktif edildi. Tekrar kullanımı engellendi. 
+                
+                btnSend.Enabled = true;             //Send button kullanılmak üzere aktif tutuldu.
+                textMessage.Focus();                //Klavyeden girilen mesajı bir yere aktarmak için kullanıldı. 
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());     //hata oluşması durumunda hatanın gösterilmesini sağladım.
+
+            }   
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            ColorDialog Renk = new ColorDialog();                   //Renk butonu oluşturuldu.Renk paleti aktif edildi.
+            Renk.ShowDialog();                                      //Renk paletinden seçilen rengin form1 üzerinden aktif olması ve görünmesi sağlandı.
+            this.BackColor = Renk.Color;
+        }
+
+        private void textMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                btnSend.PerformClick();
+            }
+        }
+
+        private void HostUserName_TextChanged(object sender, EventArgs e)
+        {
+            string YourName = HostUserName.ToString();
+        }
+
+        private void FriendUserName_TextChanged(object sender, EventArgs e)
+        {
+            string FriendName = FriendUserName.ToString();
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();    //Gönderilecek olan mesaj ASCII code tipine dönüştürüldü.
+                byte[] msg = new byte[1500];                                        //String veri tipinde yazılan mesaj byte şeklinde ayarlandı.
+                msg = enc.GetBytes(textMessage.Text);                               //textbox'dan yazılan mesajın msg değişkenine aktarımı sağlandı.
+
+                sck.Send(msg);                              //Socket üzerinden mesajın gönderilmesi sağlandı. 
+                listMessage.Items.Add(HostUserName.ToString() + " => " + textMessage.Text);      //Gönderilen mesaj listmessage kısmına aktarıldı ve host tarafından gönderildiği için mesajın başına you yazdırdık.
+                textMessage.Clear();                            //Mesaj gönderildikten sonra textbox temizlendi. 
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());                 //Gönderilen mesajın messagebox üzerinde görünmesi sssağlandı. 
+            }
+        }
+    }
+}
